57e19188bf3de21c3660f9856d48a271a3de2fad
Nico Zimmermann <hello@britzpetermann.com>
2010-11-30 12:40:55 +0100

    added showing local ip

--- Classes/ConfigViewController.h
+++ Classes/ConfigViewController.h
@@ -1,7 +1,14 @@
 #import <UIKit/UIKit.h>
+#include <ifaddrs.h>
+#include <arpa/inet.h>
 
 @interface ConfigViewController : UIViewController {
 
+	 UILabel *ipAddress;
 }
+
+@property (nonatomic, retain) IBOutlet UILabel *ipAddress;
+
+- (NSString *)getIPAddress;
 
 @end
